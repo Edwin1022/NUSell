e200e9d15c18d606f2ec270155630d96e85e155b
Edwin Wong <wongedwin93@gmail.com>
2024-06-29 01:18:34 +0800

    Deployment Bugfix 4

--- screens/UserProfileScreen.js
+++ screens/UserProfileScreen.js
@@ -60,7 +60,7 @@ const UserProfileScreen = () => {
   const fetchSellerData = async () => {
     try {
       const res = await axios.put(
-        `http://192.168.1.101:8000/users/getUserData?email=${selectedUser.email}`
+        `https://nusell.onrender.com/getUserData?email=${selectedUser.email}`
       );
       setSeller(res.data.data);
     } catch (err) {
