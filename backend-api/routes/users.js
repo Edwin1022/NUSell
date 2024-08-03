a15c1e4f6a05815eedf4af7fbab9c1f9c5068082
Edwin Wong <wongedwin93@gmail.com>
2024-08-03 16:00:55 +0800

    Deployment Bugfix 1

--- backend-api/routes/users.js
+++ backend-api/routes/users.js
@@ -299,7 +299,7 @@ router.get("/profile/:userId", async (req, res) => {
     // find the user with the given userId
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      res.status(404).json({ message: "User not found" });
     }
 
     res.status(200).json({ user });
